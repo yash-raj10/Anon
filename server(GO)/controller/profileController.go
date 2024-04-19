0b9d21b8dd525fc2ffce62aec5fd526193ce741d
yash <yashraj.se10@gmail.com>
2024-04-20 01:13:51 +0530

    fixed error

--- server(GO)/controller/profileController.go
+++ server(GO)/controller/profileController.go
@@ -10,59 +10,100 @@ import (
 	"github.com/yash-raj10/Anon/model"
 )
 
-func getProfiles(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request){
+// var db *sql.DB
+
+// func init(){
+
+// 	db, err := sql.Open("postgres", os.Getenv("DB_URL"))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer db.Close()
+
+// 	_, err = db.Exec("CREATE TABLE IF NOT EXISTED profiles (id SERIAL PRIMARY KEY, name TEXT, imageSrc TEXT, collage TEXT, social TEXT)")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
+// func GetProfiles(w http.ResponseWriter, r *http.Request) {
+// 		rows, err := db.Query("SELECT *From profiles")
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		defer rows.Close()
+
+// 		var profiles []model.Profile
+// 		// profiles = []Profile{}
+
+// 		for rows.Next() {
+// 			var p model.Profile
+// 			if err := rows.Scan(&p.Id, &p.Name, &p.ImageSrc, &p.Collage, &p.Social); err != nil {
+// 				log.Fatal(err)
+// 			}
+// 			profiles = append(profiles, p)
+// 		}
+// 		if err := rows.Err(); err != nil {
+// 			log.Fatal(err)
+// 		}
+
+// 		json.NewEncoder(w).Encode(profiles)
+
+// }
+
+func GetProfiles(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		rows, err := db.Query("SELECT *From profiles")
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 		}
 		defer rows.Close()
 
-	var profiles []model.Profile
-	// profiles = []Profile{}
+		var profiles []model.Profile
+		// profiles = []Profile{}
 
-	for rows.Next(){
-		var p model.Profile
-		if err :=  rows.Scan(&p.Id, &p.Name, &p.ImageSrc, &p.Collage, & p.Social); err  != nil {
+		for rows.Next() {
+			var p model.Profile
+			if err := rows.Scan(&p.Id, &p.Name, &p.ImageSrc, &p.Collage, &p.Social); err != nil {
+				log.Fatal(err)
+			}
+			profiles = append(profiles, p)
+		}
+		if err := rows.Err(); err != nil {
 			log.Fatal(err)
 		}
-		profiles = append(profiles, p)
-	}
-	if err := rows.Err(); err != nil{
-       log.Fatal(err)
-	}
 
-	json.NewEncoder(w).Encode(profiles)
+		json.NewEncoder(w).Encode(profiles)
 	}
 }
 
-func getProfile(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request){
+func GetProfile(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		id := vars["id"]
 
 		var p model.Profile
 
-		err := db.QueryRow("SELECT *From profiles WHERE id = $1", id).Scan(&p.Id, &p.Name, &p.ImageSrc, &p.Collage, & p.Social)
-		if err != nil{
-		w.WriteHeader(http.StatusNotFound)
-		return
+		err := db.QueryRow("SELECT *From profiles WHERE id = $1", id).Scan(&p.Id, &p.Name, &p.ImageSrc, &p.Collage, &p.Social)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
 		}
 
-	json.NewEncoder(w).Encode(p)
+		json.NewEncoder(w).Encode(p)
 	}
 }
 
-func createProfile(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request){
+func CreateProfile(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		var p model.Profile
 		json.NewDecoder(r.Body).Decode(&p)
 
 		err := db.QueryRow("INSERT INTO profiles (name, imageSrc, collage, social) VALUES ($1, $2, $3, $4) RETURNING id", p.Name, p.ImageSrc, p.Collage, p.Social).Scan(&p.Id)
-		if err != nil{
-		    log.Fatal(err)
+		if err != nil {
+			log.Fatal(err)
 		}
 
-	json.NewEncoder(w).Encode(p)
+		json.NewEncoder(w).Encode(p)
 	}
 }
--- server(GO)/main.go
+++ server(GO)/main.go
@@ -10,25 +10,25 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"github.com/yash-raj10/Anon/controller"
 )
 
-func main(){
+func main() {
 	db, err := sql.Open("postgres", os.Getenv("DB_URL"))
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTED profiles (id SERIAL PRIMARY KEY, name TEXT, imageSrc TEXT, collage TEXT, social TEXT)")
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	r := mux.NewRouter()
-	r.HandleFunc("/apiV1/profiles", getProfiles(db)).Methods("GET")
-	r.HandleFunc("/apiV1/profile", createProfile(db)).Methods("POST")
-	r.HandleFunc("/apiV1/profiles/{id}", getProfile(db)).Methods("GET")
-	
+	r.HandleFunc("/apiV1/profiles", controller.GetProfile(db)).Methods("GET")
+	r.HandleFunc("/apiV1/profile", controller.CreateProfile(db)).Methods("POST")
+	r.HandleFunc("/apiV1/profiles/{id}", controller.CreateProfile(db)).Methods("GET")
 
 	cors := handlers.CORS(
 		handlers.AllowedHeaders([]string{"Content-Type"}),
