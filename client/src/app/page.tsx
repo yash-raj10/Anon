e66bc3c0b9849980cec9b667b0ba57c7d7c05a60
yash <yashraj.se10@gmail.com>
2024-07-05 11:11:47 +0530

    fixed

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,12 @@
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 
 export default function Home() {
+  const { userId } = auth();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-20">
-      hlo
+      <div>{userId}</div>
     </main>
   );
 }
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { BiNetworkChart } from "react-icons/bi";
 import { BsIncognito } from "react-icons/bs";
-import { UserButton, auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 import Image from "next/image";
 
 export default async function Navbar() {
-  const { userId } = auth();
-
   return (
     <div className="navbar backdrop-blur bg-base-100/30 max-w-7xl border-2 rounded-3xl z-50">
       <div className="w-full flex justify-between items-center">
         <div className="">
           <a className="btn btn-ghost text-xl">
-            <Image
-              src="https://raw.githubusercontent.com/yash-raj10/bookmart/main/public/DEQUIZZ.png"
-              height={40}
-              width={40}
-            />
+            <BsIncognito size={35} />
           </a>
         </div>
 
@@ -27,22 +28,18 @@ export default async function Navbar() {
         </div>
 
         <div className="flex-none gap-2 mr-3">
-          <BsIncognito size={40} />
+          <SignedOut>
+            <div className="btn btn-neutral">
+              {" "}
+              <SignInButton />
+            </div>
+          </SignedOut>
+          <SignedIn>
+            <div className="">
+              <UserButton />
+            </div>
+          </SignedIn>
         </div>
-
-        {/* <div>
-          {userId ? (
-            <div className="flex gap-4 items-center">
-              <Link href="/dashboard">Dashboard</Link>
-              <UserButton afterSignOutUrl="/" />
-            </div>
-          ) : (
-            <div className="flex gap-4 items-center">
-              <Link href="/sign-up">Sign up</Link>
-              <Link href="/sign-in">Sign In</Link>
-            </div>
-          )}
-        </div> */}
       </div>
     </div>
   );
